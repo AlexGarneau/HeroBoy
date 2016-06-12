13d6235ce1be06a614eec334b0247054e730560c
Karl Garneau <android007@gmail.com>
2016-06-12 01:47:15 -0600

    Added files Alex will be modifying.

---
+++ Assets/Scenes/Hero Boy Levels/Act One/School/TutorialAssets/AttackTutorialScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackTutorialScript : MonoBehaviour
+{
+	Animator _anim;
+
+	void Start ()
+	{
+		_anim = GetComponent<Animator> ();
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Z)) {
+			_anim.SetTrigger ("ZPressed");
+		}
+		if (Input.GetKeyDown (KeyCode.X)) {
+			_anim.SetTrigger ("XPressed");
+		}
+		if (Input.GetKeyDown (KeyCode.C)) {
+			_anim.SetTrigger ("CPressed");
+		}
+		if (Input.GetKeyDown (KeyCode.V)) {
+			_anim.SetTrigger ("VPressed");
+		}
+	}
+}
