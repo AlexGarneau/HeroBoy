e066269f0f78ad184453f6eed3e24d9d1a21d4ed
Karl Garneau <karlgarneau@gmail.com>
2016-07-18 13:17:57 -0600

    Changes to Menu for ConBravo Build

--- Assets/Scripts/Menus/StartMenu.cs
+++ Assets/Scripts/Menus/StartMenu.cs
@@ -6,68 +6,83 @@ public class StartMenu : MonoBehaviour
 {
     public GlobalControl gc;
 
-	public GameObject titleScreen;
-	public GameObject optionsScreen;
+    public GameObject titleScreen;
+    public GameObject optionsScreen;
+    public GameObject levelSelectScreen;
 
-	public GameObject title;
-	public Button start;
-	public Button options;
-	public Button quit;
+    public GameObject title;
+    public Button start;
+    public Button options;
+    public Button quit;
 
-	public Button resolutionOp1;
-	public Button resolutionOp2;
-	public Button resolutionOp3;
-	
-	public Button fpsOp1;
-	public Button fpsOp2;
+    public Button resolutionOp1;
+    public Button resolutionOp2;
+    public Button resolutionOp3;
 
-	public Button fsOp1;
-	public Button fsOp2;
+    public Button fpsOp1;
+    public Button fpsOp2;
 
-	void Start ()
-	{
+    public Button fsOp1;
+    public Button fsOp2;
+
+    public Button levelSelect1;
+    public Button levelSelect2;
+    public Button levelSelect3;
+    public Button levelSelect4;
+
+    void Start()
+    {
         // TODO: Check this out for research potential: http://docs.unity3d.com/Manual/MobileOptimisation.html
-        start = start.GetComponent<Button> ();
-		options = options.GetComponent<Button> ();
-		quit = quit.GetComponent<Button> ();
+        start = start.GetComponent<Button>();
+        options = options.GetComponent<Button>();
+        quit = quit.GetComponent<Button>();
 
-		resolutionOp1 = resolutionOp1.GetComponent<Button> ();
-		resolutionOp2 = resolutionOp1.GetComponent<Button> ();
-		resolutionOp3 = resolutionOp1.GetComponent<Button> ();
+        resolutionOp1 = resolutionOp1.GetComponent<Button>();
+        resolutionOp2 = resolutionOp1.GetComponent<Button>();
+        resolutionOp3 = resolutionOp1.GetComponent<Button>();
 
-		fpsOp1 = fpsOp1.GetComponent<Button> ();
-		fpsOp2 = fpsOp2.GetComponent<Button> ();
-		
-		fsOp1 = fsOp1.GetComponent<Button> ();
-		fsOp2 = fsOp2.GetComponent<Button> ();
+        fpsOp1 = fpsOp1.GetComponent<Button>();
+        fpsOp2 = fpsOp2.GetComponent<Button>();
 
-		Application.targetFrameRate = 60;
+        fsOp1 = fsOp1.GetComponent<Button>();
+        fsOp2 = fsOp2.GetComponent<Button>();
 
-		HideOptions ();
-	}
+        Application.targetFrameRate = 60;
 
-	public void StartLevel ()
-	{
+        BackToTitle();
+    }
+
+    public void StartLevel()
+    {
         Time.timeScale = 1;
-        // Starrt fresh.
+        // Start fresh.
         if (GlobalControl.instance != null)
         {
             GlobalControl.instance.resetPlayerStats();
         }
-		Application.LoadLevel (7);
-	}
+        Application.LoadLevel(7);
+    }
+
+    public void ShowLevelSelect()
+    {
+        titleScreen.SetActive(false);
+        optionsScreen.SetActive(false);
+        levelSelectScreen.SetActive(true);
+    }
 
 	public void ShowOptions ()
 	{
 		titleScreen.SetActive (false);
 		optionsScreen.SetActive (true);
-	}
+        levelSelectScreen.SetActive(false);
+    }
 
-	public void HideOptions ()
+	public void BackToTitle ()
 	{
 		titleScreen.SetActive (true);
 		optionsScreen.SetActive (false);
-	}
+        levelSelectScreen.SetActive(false);
+    }
 
 	public void SetResolution800x600 ()
 	{
