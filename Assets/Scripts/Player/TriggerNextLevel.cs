f306fad8df3bbe0ee34faefe4279fcb7ae452fd7
Karl Garneau <karlgarneau@gmail.com>
2016-07-25 21:35:51 -0600

    DC Escape levels prepared, still requires functioning enemies.

---
+++ Assets/Scripts/Player/TriggerNextLevel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerNextLevel : MonoBehaviour {
+
+    GameObject player;
+
+    void start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject == player)
+        {
+            Application.LoadLevel(Application.loadedLevel + 1);
+        }
+    }
+}
