b6f997f112e90661346eedc0dd446f117a5cbda2
Karl Garneau <karlgarneau@gmail.com>
2016-08-13 11:17:15 -0600

    Minor changes

--- Assets/GameControllerSnakesAndLadders.cs
+++ Assets/GameControllerSnakesAndLadders.cs
@@ -9,6 +9,8 @@ public class GameControllerSnakesAndLadders : MonoBehaviour {
     public GameObject player3;
     public GameObject player4;
 
+    public int nextLevel = 1;
+
     public enum WhoseTurn
     {
         p1,
@@ -18,14 +20,71 @@ public class GameControllerSnakesAndLadders : MonoBehaviour {
         end
     }
     ;
+    protected WhoseTurn currentPlayer;
 
-    // Use this for initialization
     void Start () {
-	
+	    
 	}
 	
-	// Update is called once per frame
 	void Update () {
-	
+	    
 	}
+
+    void RollDice(int dieValue)
+    {
+        dieValue = die.currentValue;
+    }
+
+
+    void SetTurn(WhoseTurn player)
+    {
+        switch (player)
+        {
+            case WhoseTurn.p1:
+
+                break;
+            case WhoseTurn.p2:
+                StartCoroutine(PrepareRollDie());
+                break;
+            case WhoseTurn.p3:
+                StartCoroutine(PrepareRollDie());
+                break;
+            case WhoseTurn.p4:
+                StartCoroutine(PrepareRollDie());
+                break;
+            case WhoseTurn.end:
+                StartCoroutine(PrepareEnd());
+                break;
+        }
+        currentPlayer = player;
+    }
+
+    IEnumerator PrepareRollDie ()
+    {
+        yield return new WaitForSeconds(1.5f);
+        RollDice(die.currentValue);
+    }
+
+    IEnumerator PrepareEnd ()
+    {
+        yield return new WaitForSeconds(1.5f);
+        Application.LoadLevel(nextLevel);
+    }
+
+    void EndTurn()
+    {
+        if(currentPlayer == WhoseTurn.p1)
+        {
+            SetTurn(WhoseTurn.p2);
+        } else if (currentPlayer == WhoseTurn.p2)
+        {
+            SetTurn(WhoseTurn.p3);
+        } else if (currentPlayer == WhoseTurn.p3)
+        {
+            SetTurn(WhoseTurn.p4);
+        } else if (currentPlayer == WhoseTurn.p4)
+        {
+            SetTurn(WhoseTurn.p1);
+        }
+    }
 }
