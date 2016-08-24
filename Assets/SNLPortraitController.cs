e5bf59ef904d23e2ad9c858c9b9f709ba6b32156
Karl Garneau <karlgarneau@gmail.com>
2016-08-24 14:58:30 -0600

    Added character portrait placeholders, modified Die physics values for Snakes and Ladders

---
+++ Assets/SNLPortraitController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SNLPortraitController : MonoBehaviour {
+
+    Animator anim;
+    public SNLPlayer player;
+
+    /*The goal of this script is to read the assigned player's position relative to the other players. If they are in the lead, the animation plays "Winning". If in last place, "Losing". If neither, "Normal".*/
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
