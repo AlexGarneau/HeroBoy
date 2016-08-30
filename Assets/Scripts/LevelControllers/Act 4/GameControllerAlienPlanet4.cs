245f16b6e5c3921ba18e7842adc49d5a9319f3fe
Karl Garneau <karlgarneau@gmail.com>
2016-08-30 14:40:25 -0600

    Set up Clown Levels, added respective LevelControllers

---
+++ Assets/Scripts/LevelControllers/Act 4/GameControllerAlienPlanet4.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControllerAlienPlanet4 : AbstractGameController
+{
+	// Use this for initialization
+	public override void Start ()
+	{
+		base.Start ();
+		enemyCount = 6;
+		// TODO: Get references to all the SpawnZombie objects currently in the level.
+
+		LevelBoundary.topWidth = 8.103892f - -4.385192f; // Use the enemies positions during game play to get these coordinates.
+		LevelBoundary.bottomWidth = 8.103892f - -4.385192f; // Widths are right-corner minus left corner.
+		LevelBoundary.left = -4.385192f; // Left is the left corner of the larger width (in this case, bottom)
+		LevelBoundary.bottom = -3.9f; // Bottom is the lowest point in the boundary.
+		LevelBoundary.height = 0.7f - -3.7f; // Height is top minus bottom.
+	}
+}
